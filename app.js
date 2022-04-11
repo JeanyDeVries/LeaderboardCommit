ccefaad49857ad0c8ee728f21587deb38f5497cb
Jeany de Vries <jeanydevries@hotmail.com>
2022-04-11 11:57:23 +0200

    base html

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 require('dotenv').config()
 const express = require('express')
-//const indexRoute = require('./routes/index')
+const indexRoute = require('./routes/index')
 //const projectsRoute = require('./routes/projects')
 
 module.exports = express()
   .set('view engine', 'ejs')
   .set('views', './views')
 
-  .use(express.static('./public'))
-
-  //.use('/', indexRoute)
+  .use('/', indexRoute)
   //.use('/projects', projectsRoute)
