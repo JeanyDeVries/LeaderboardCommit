0fb42410a9f2b569b33a77aa1a984752d40a423b
Jeany de Vries <jeanydevries@hotmail.com>
2022-04-11 14:30:16 +0200

    search querie in index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,65 @@
 const express = require('express')
 
+const { graphql } = require('@octokit/graphql')
+const graphqlAuth = graphql.defaults({
+  headers: { authorization: 'token ' + process.env.key },
+})
+
 module.exports = express
   .Router()
 
   .get('/', function (req, res) {
-    res.render('index', {})
+    // Get the repository information from my GitHub account
+    graphqlAuth(`{
+    user(login: "JeanyDeVries") 
+    {
+      organization(login: "cmda-minor-web") 
+      {
+        repositories(last: 20, orderBy: {field: CREATED_AT, direction: ASC}) 
+        {
+          edges 
+          {
+            node 
+            {
+              forks(first: 10) 
+              {
+                edges {
+                  node 
+                  {
+                    name
+                    refs(refPrefix: "refs/heads/", first: 100)
+                     {
+                      edges 
+                      {
+                        node 
+                        {
+                          name
+                          target 
+                          {
+                            ... on Commit 
+                            {
+                              id
+                              history(first: 0) 
+                              {
+                                totalCount
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  `).then((data) => {
+      res.render('index', {
+        
+      })
+    })
   })
