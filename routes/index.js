71acdacc9180ef754a480ada4d5d112b981d6b3d
Jeany de Vries <jeanydevries@hotmail.com>
2022-04-13 14:24:11 +0200

    shows data on subject page

--- routes/index.js
+++ routes/index.js
@@ -119,7 +119,8 @@ module.exports = express
     }
   `).then((data) => {
       res.render('detail', {
-        
+          dataRepo: data.organization.repository,
+          nameRepo: nameSubject
       })
     })
 
