2f30a7978ad446de9723ece9e45a2ae40b192b65
gekkeboyjeff <jeffrey.ullers@hva.nl>
2022-04-13 14:27:41 +0200

    Render j3js data

--- public/script/script.js
+++ public/script/script.js
@@ -1,13 +1,21 @@
-var http = new XMLHttpRequest();
-console.log(http)
-var data = (http.responseText)
-console.log(data)
+d3.json('topPerSubject.json').then((data) => {
+    console.log(data.flat()) // gooit alles in 1 array
 
-/*
-d3.json('topPerSubject.json', (data) =>{
-    var canvasMain = d3.select('body').append('svg')
-    .attr('width:', 300)
-    .attr('height:', 500)
-    console.log(data)
+    var hoi = JSON.parse(data)
+    console.log(hoi)
+    // const homeChart = d3.select('section:first-of-type')
+        // .selectAll('p')
+        // .data(data.flat())
+        // .enter()
+        // .append('p')
+        // .text(data => data.subject)
+
+//         const bars = homeChart
+// .selectAll('.bar')
+// .data(data.flat())
+// .enter()
+// .append('div')
+// .classed('bar', true);
+
 })
-*/
+
--- routes/index.js
+++ routes/index.js
@@ -96,6 +96,5 @@ module.exports = express
         subjects: data.dataSubject,
         names: subjectNames
       })
-      res.json(data)
     })
   })
