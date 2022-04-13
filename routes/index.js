198859f04916a3b238359b2170b204cb3629cd6a
Jeany de Vries <jeanydevries@hotmail.com>
2022-04-13 09:29:43 +0200

    forloop per subject

--- routes/index.js
+++ routes/index.js
@@ -14,76 +14,62 @@ module.exports = express
     // Get the repository information from my GitHub account
     graphqlAuth(`
     {
-      repository(
-        owner: "cmda-minor-web"
-        name: "browser-technologies-2122"
-      ) {
-        name
-        forkCount
-        forks(
-          first: 50
-          orderBy: { field: NAME, direction: DESC }
-        ) {
-          totalCount
+      organization(login: "cmda-minor-web") {
+        repositories(orderBy: {field: UPDATED_AT, direction: DESC}, first: 20) {
           nodes {
             name
-            refs(refPrefix: "refs/heads/", first: 1) {
-                          edges {
-                            node {
-                              name
-                              target {
-                                ... on Commit {
-                                  history(first: 0) {
-                                    totalCount
-                                  }
-                                  author {
-                                    name
-                                  }
-                                }
-                              }
-                            }
-                          }
-                        }
           }
         }
       }
     }
   `).then((data) => {
-    console.log(data.repository.forks)
-    /*
-      var subjects = data.organization.repositories.edges;
-      var data = {dataSubject:[]};
-      var topPerSubject = []
-      var subjectNames = []
-
-      subjects.forEach(subject => { 
-          var group = []
-          var dataTop = [];
-          var subjectName = subject.node.name;
-          subjectNames.push(subjectName);
-          
-          var repositories = subject.node.forks.edges;
-          repositories.forEach(repository => 
-          { 
-              var references = repository.node.refs.edges
-              references.forEach(reference => 
-              {  
-                  var commitCount = reference.node.target.history.totalCount;
-                  var user = reference.node.target.author.name;
-                  group.push({subject: subjectName, user: user, commits: commitCount})
-              })
-          })
-          dataTop = group.sort((a,b)=> b.commits-a.commits)
-          data.dataSubject.push(group)
-          topPerSubject.push(dataTop)
-      }) 
-
-      res.render('index', {
-        subjects: data.dataSubject,
-        names: subjectNames
-      })
-      console.log(subjects)
-    */
+      var repositories = data.organization.repositories.nodes;
+      repositories.forEach(subject => {
+        loadData(subject.name)
+      });
     })
   })
+
+  function loadData(subjectName){
+    module.exports = express
+      .Router()
+      .get('/', function (req, res) {
+        // Get the repository information from my GitHub account
+        graphqlAuth(`
+        {
+          repository(owner: "cmda-minor-web", name: ${subjectName}) {
+            name
+            forkCount
+            forks(first: 50, orderBy: {field: NAME, direction: DESC}) {
+              totalCount
+              nodes {
+                name
+                refs(refPrefix: "refs/heads/", first: 1) {
+                  edges {
+                    node {
+                      name
+                      target {
+                        ... on Commit {
+                          history(first: 0) {
+                            totalCount
+                          }
+                          author {
+                            name
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `).then((data) => {
+          //set data in JSON file which will be read later on for the html rendering
+          console.log(data)
+        })
+      })
+
+  }
 
